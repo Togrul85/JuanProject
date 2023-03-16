f506ce48746cec9b15e51ffb19b39e2043facc2a
Togrul85 <toghrulkhs@code.edu.az>
2023-03-16 13:34:00 +0400

    CommentsPart2

--- JuanProject/Controllers/BlogController.cs
+++ JuanProject/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using JuanProject.DAL;
 using JuanProject.Models;
 using JuanProject.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,10 +10,12 @@ namespace JuanProject.Controllers
     public class BlogController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
 
-        public BlogController(AppDbContext context)
+        public BlogController(AppDbContext context, UserManager<AppUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -25,12 +28,34 @@ namespace JuanProject.Controllers
             {
                 Blog = _context.Blogs
                 .Include(b=>b.Comments)
+                .ThenInclude(c=>c.AppUser)
                 .FirstOrDefault(b => b.Id == id),
                 Blogs = _context.Blogs.OrderByDescending(b => b.Id).Take(2).ToList()
 
             };
        
             return View(blogDetailVM);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> AddComment(string commentMessage,int blogId)
+        {
+            AppUser user = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                 user = await _userManager.FindByNameAsync(User.Identity.Name);
+            }
+            else
+            {
+                return RedirectToAction("login", "account");
+            }
+            Comment comment = new();
+            comment.CreatedDate = DateTime.Now;
+            comment.AppUserId =user.Id;
+            comment.BlogId = blogId;
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+            return RedirectToAction("detail",new {id=blogId});
         }
     }
 }
